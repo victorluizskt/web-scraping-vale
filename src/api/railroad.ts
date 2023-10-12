3aed77e40c153c6be52f0f30439185f2254af128
victor luiz <victorluizcefet@gmail.com>
2023-10-11 22:19:03 -0300

    update component

--- src/api/railroad.ts
+++ src/api/railroad.ts
@@ -1,32 +1,62 @@
 import api from './base';
 
 interface RailwayLocation {
-    excessao: null,
-    locaisFerroviarios: {
-        codigo: string, 
-        codigoIBGE: string, 
-        descricaoDetalhada: string, 
-        descricaoInternet: string, 
-        ferroviaOperadora: string, 
-        ferroviaProprietaria: string, 
-        id: number, 
-        sequencia: any, 
-        unidadeFederacao: string
-    }[];
+    message: {
+        excessao: null,
+        locaisFerroviarios: {
+            codigo: string, 
+            codigoIBGE: string, 
+            descricaoDetalhada: string, 
+            descricaoInternet: string, 
+            ferroviaOperadora: string, 
+            ferroviaProprietaria: string, 
+            id: number, 
+            sequencia: any, 
+            unidadeFederacao: string
+        }[];
+    }
 };
 
-export const getRailwayLocations = async (railroadCode?: string): Promise<RailwayLocation[]> => {
+interface ClassPassageSearch {
+    message: {
+        excessao: null,
+        classesPassagem: {
+            codigoFerrovia: string;
+            id: Int32Array;
+            indicadorClasseCadeirante: string;
+            nome: string;
+        }[]; 
+    }
+}
+
+export const getRailwayLocations = async (railroadCode?: string): Promise<RailwayLocation> => {
     try {
         const params = {
             codigoFerrovia: railroadCode,
         }
         const response = await api.post('/obterLocaisFerroviarios', params);
         if(response.status == 200) {
-            const railwayLocations:  RailwayLocation[] = response.data;
+            const railwayLocations:  RailwayLocation = response.data;
             return railwayLocations;
         };
         throw new Error('Failed to get informations.');
     } catch(error) {
         throw error;
     };
-}
+};
+ 
+export const getClassPassageSearch = async (railroadCode?: string): Promise<ClassPassageSearch> => {
+    try {
+        const params = {
+            codigoFerrovia: railroadCode,
+        }
+        const response = await api.post('/pesquisaClassePassagem', params);
+        if(response.status == 200) {
+            const railwayLocations:  ClassPassageSearch = response.data;
+            return railwayLocations;
+        };
+        throw new Error('Failed to get informations.');
+    } catch(error) {
+        throw error;
+    };
+};
--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,25 +1,54 @@
 import { useState } from 'react';
-import { getRailwayLocations } from '../../api/railroad';
+import { getRailwayLocations, getClassPassageSearch } from '../../api/railroad';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
 
 const railroads = [
     { title: 'ESTRADA DE FERRO CARAJAS (MA-PA)', id: "04" },
     { title: 'ESTRADA DE FERRO VITORIA A MINAS', id: "03" },
 ];
 
+interface Locale {
+    codigo: string, 
+    codigoIBGE: string, 
+    descricaoDetalhada: string, 
+    descricaoInternet: string, 
+    ferroviaOperadora: string, 
+    ferroviaProprietaria: string, 
+    id: number, 
+    sequencia: any, 
+    unidadeFederacao: string
+};
+
+interface ClassPassage {
+    codigoFerrovia: string;
+    id: Int32Array;
+    indicadorClasseCadeirante: string;
+    nome: string;
+}
+
 export default function SelectSmall() {
   const [railroad, setRailroad] = useState(String);
+  const [selectedOrigin, setSelectedOrigin] = useState(String);
+  const [classPassage, setClassPassage] = useState(Object);
+  const [selectedDest, setSelectedDest] = useState(String);
+  const [classPassageSelect, setClassPassageSelect] = useState(String);
   const [origin, setOrigin] = useState(Object);
   const handleChange = async (event: SelectChangeEvent) => {
     const idWagon = railroads.find(railroad => railroad.title === event.target.value)?.id;
     const response = await getRailwayLocations(idWagon);
-    setOrigin(response);
+    const classPassage = await getClassPassageSearch(idWagon);
+    setOrigin(response.message.locaisFerroviarios);
+    setClassPassage(classPassage.message.classesPassagem);
   };
 
-  console.log(origin);
+  console.log(classPassage);
 
   return (
     <>
@@ -34,6 +63,7 @@ export default function SelectSmall() {
                 id="demo-simple-select-standard"
                 value={railroad}
                 onChange={(e) => handleChange(e)}
+                disabled={origin.length === 0}
             >
             {railroads.map(railroad => (
                 <MenuItem 
@@ -45,25 +75,101 @@ export default function SelectSmall() {
             ))}
         </Select>
         </FormControl>
-        {/* <FormControl 
+       <div style={{ display: 'flex' }}>
+            <FormControl 
+                variant="standard" 
+                sx={{ m: 1, minWidth: 120, width: '100%' }} 
+                size="small"
+            >
+                <InputLabel id="demo-simple-select-standard-label">Origem</InputLabel>
+                <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={selectedDest}
+                    disabled={origin.length === 0}
+                >
+                {origin.length > 0 && origin.map((dest: Locale) => (
+                    <MenuItem 
+                        value={dest.descricaoInternet}
+                        onClick={() => setSelectedDest(dest.descricaoInternet)}
+                    >
+                        {dest.descricaoInternet}
+                    </MenuItem>
+                ))}
+                </Select>
+                </FormControl>
+            <FormControl 
+                variant="standard" 
+                sx={{ m: 1, minWidth: 120, width: '100%' }} 
+                size="small"
+            >
+                <InputLabel id="demo-simple-select-standard-label">Destino</InputLabel>
+                <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={selectedOrigin}
+                    disabled={!origin}
+                >
+                {origin.length > 0 && origin.map((dest: Locale) => (
+                    <MenuItem 
+                        value={dest.descricaoInternet}
+                        onClick={() => setSelectedOrigin(dest.descricaoInternet)}
+                    >
+                        {dest.descricaoInternet}
+                    </MenuItem>
+                ))}
+                </Select>
+        </FormControl>
+       </div>
+       <div>
+       <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label" />
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+            >
+                <div>
+                <FormControlLabel value="female" control={<Radio />} label="Ida e volta" />
+                <FormControlLabel value="male" control={<Radio />} label="So ida" />
+                </div>
+            </RadioGroup>
+        </FormControl>
+       </div>
+       <div style={{ display: 'flex', width: '100%'}}>
+        <input
+            style={{ marginRight: '5px', width: '100%'}}
+            id="date" 
+            type="date" 
+        />
+        <input
+            id="date" 
+            type="date"
+            style={{ marginLeft: '5px', width: '100%'}}
+        />        
+       </div>
+       <FormControl 
             variant="standard" 
             sx={{ m: 1, minWidth: 120, width: '100%' }} 
             size="small"
         >
-            <InputLabel id="demo-simple-select-standard-label">Origem</InputLabel>
+            <InputLabel id="demo-simple-select-standard-label">Ferrovia</InputLabel>
             <Select
                 labelId="demo-simple-select-standard-label"
                 id="demo-simple-select-standard"
-                value={railroad}
-                onChange={handleChange}
+                value={classPassageSelect}
+                disabled={origin.length === 0}
             >
-            {railroads.map(railroad => (
-                <MenuItem value={railroad.id}>
-                    {railroad.title}
+            {classPassage.length > 0 && classPassage.map((dest: ClassPassage) => (
+                <MenuItem 
+                    value={dest.nome}
+                    onClick={() => setClassPassageSelect(dest.nome)}
+                >
+                    {dest.nome}
                 </MenuItem>
             ))}
         </Select>
-        </FormControl> */}
+        </FormControl>
     </>
   );
 }
